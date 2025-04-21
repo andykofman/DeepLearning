376f0ec67e98e23f6074e1709080372ea831804f
A.Ali <ali.a@aucegypt.edu>
2025-04-21 16:11:05 +0200

    Changed from boston dataset to California

--- Chap 2  final/code.py
+++ Chap 2  final/code.py
@@ -11,12 +11,12 @@ np.set_printoptions(precision=4)
 
 TEST_ALL = False
 
-# Boston data
-from sklearn.datasets import load_boston
-boston = load_boston()
-data = boston.data
-target = boston.target
-features = boston.features_names
+# California housing data
+from sklearn.datasets import fetch_california_housing
+housing = fetch_california_housing()
+data = housing.data
+target = housing.target
+features = housing.feature_names
 
 """
 Scikit Learn Linear Regression
@@ -28,4 +28,29 @@ data = s.fit_transform(data)
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(data,target,test_size = 0.3, random_state = 80718)
-y_test = y_train.reshape(-1,1), y_test.reshape(-1,1)
+#The -1 in reshape means "automatically calculate this dimension" 
+# while 1 ensures we have a 2D array with one column
+# Reshape y values to 2D arrays
+y_train = y_train.reshape(-1,1)
+y_test = y_test.reshape(-1,1)
+
+"""
+Step 2: Linear Regression
+"""
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression(fit_intercept=True)
+lr.fit(X_train,y_train)
+preds = lr.predict (X_test)
+
+# Visualization
+# Visualization
+import matplotlib.pyplot as plt
+plt.xlabel("Predicted value")
+plt.ylabel("Actual value")
+plt.title("Predicted vs. Actual values for\nLinear Regression model")
+max_val = max(max(preds), max(y_test))
+plt.xlim([0, max_val])
+plt.ylim([0, max_val])
+plt.scatter(preds, y_test)
+plt.plot([0, max_val], [0, max_val])
+plt.show()
